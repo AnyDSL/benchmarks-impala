8b251187eaad7c86a2ca337d9e8185664f3478cd
Roland Leißa <leissa@cs.uni-saarland.de>
2014-08-10 19:14:55 +0200

    added fasta.sh ghc instructions

--- fasta.sh
+++ fasta.sh
@@ -5,3 +5,15 @@ rm fasta.gcc-5.c
 
 COMMAND LINE:
 ./fasta.gcc-5.gcc_run 25000000
+
+
+MAKE:
+mv fasta.ghc-2.ghc fasta.ghc-2.hs
+/usr/local/src/ghc-7.8.2/bin/ghc --make -fllvm -O2 -XBangPatterns -rtsopts -XOverloadedStrings fasta.ghc-2.hs -o fasta.ghc-2.ghc_run
+[1 of 1] Compiling Main             ( fasta.ghc-2.hs, fasta.ghc-2.o )
+Linking fasta.ghc-2.ghc_run ...
+rm fasta.ghc-2.hs
+1.28s to complete and log all make actions
+
+COMMAND LINE:
+./fasta.ghc-2.ghc_run  25000000
