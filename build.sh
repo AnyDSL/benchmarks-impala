3802d3028a4d14f12f99954c2e4497d7705297be
Roland Leißa <leissa@cs.uni-saarland.de>
2014-08-20 14:22:15 +0200

    using .out suffix for executables

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ clang -O2 lib.c -c
 
 for F in fannkuch nbody
 do
-    echo ">>> building ${F}_c"
-    clang -O2 ${F}.c -o ${F}_c -lm
+    echo ">>> building ${F}_c.out"
+    clang -O2 ${F}.c -o ${F}_c.out -lm
 
-    echo ">>> building ${F}_impala"
+    echo ">>> building ${F}_impala.out"
     impala ${F}.impala -emit-llvm
-    clang -O2 lib.o ${F}.bc -lm -o ${F}_impala
+    clang -O2 lib.o ${F}.bc -lm -o ${F}_impala.out
 done
