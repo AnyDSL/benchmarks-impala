0dc00b4ae3340e2ed4521af1c23ae96c842c65a5
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-08-23 17:28:42 +0200

    more work on meteor -> impala

--- lib.c
+++ lib.c
@@ -14,6 +14,10 @@ void print_f64(double d) {
    printf("%.9f\n", d);
 }
 
+void println() {
+   printf("\n");
+}
+
 // header for mandelbrot bitmap
 void print_header(int w, int h) {
    printf("P4\n%d %d\n",w,h);
@@ -28,3 +32,15 @@ void* thorin_malloc(size_t size) {
     posix_memalign(&p, 64, size);
     return p;
 }
+
+// meteor printing
+void print_meteor_scnt(int cnt) {
+   printf("%d solutions found\n\n", cnt);
+}
+
+void print_meteor_lines(char a0, char a1, char a2, char a3, char a4, char a5,
+      char a6, char a7, char a8, char a9) {
+   printf("%c %c %c %c %c \n %c %c %c %c %c \n", a0+'0', a1+'0', a2+'0',
+      a3+'0', a4+'0', a5+'0', a6+'0', a7+'0', a8+'0', a9+'0');
+}
+
