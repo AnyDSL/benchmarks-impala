dd597470f43302c76c5127296e5cf84b2172149c
Roland Leißa <leissa@cs.uni-saarland.de>
2014-08-25 17:35:34 +0200

    use c99

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ clang -O2 lib.c -c
 for F in fannkuch mandelbrot meteor nbody spectral
 do
     echo ">>> building ${F}_c.out"
-    clang -O2 ${F}.c -o ${F}_c.out -lm
+    clang -O2 -std=c99 ${F}.c -o ${F}_c.out -lm
 
     echo ">>> building ${F}_impala.out"
     impala ${F}.impala -emit-llvm
