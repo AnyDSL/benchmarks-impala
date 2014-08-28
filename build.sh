ab73849e3e90874e1b79dd4e0249d4532f4a11c4
Richard Membarth <richard.membarth@uni-saarland.de>
2014-08-28 11:54:30 +0200

    Support for Mac OS X (and macports).

--- build.sh
+++ build.sh
@@ -2,14 +2,25 @@
 
 clang -O3 lib.c -c
 
+case $OSTYPE in
+    darwin*)
+        LD_FLAGS="-L /opt/local/lib"
+        INC_FLAGS="-I /opt/local/include"
+        ;;
+    *)
+        LD_FLAGS=
+        INC_FLAGS=
+        ;;
+esac
+
 for F in aobench fannkuch fasta mandelbrot meteor nbody pidigits spectral regex
 do
     echo ">>> building ${F}_c.out"
-    clang -O3 -std=gnu99 ${F}.c -o ${F}_c.out -lm -lpcre -lgmp -s
+    clang -O3 -std=gnu99 ${INC_FLAGS} ${F}.c -o ${F}_c.out ${LD_FLAGS} -lm -lpcre -lgmp
 
     echo ">>> building ${F}_impala.out"
     impala ${F}.impala -emit-llvm
-    clang -O3 lib.o ${F}.bc -lm -lpcre -lgmp -o ${F}_impala.out
+    clang -O3 lib.o ${F}.bc ${LD_FLAGS} -lm -lpcre -lgmp -o ${F}_impala.out
 
     #echo ">>> building ${F}_rs.out"
     #rustc --opt-level 3 ${F}.rs -o ${F}_rs.out
